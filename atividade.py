4739c7e59def7e78e017510ebd1710cbfaf3c32a
Linguine1991 <tgatga121261@gmail.com>
2026-03-05 08:43:29 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,6 @@
+# Lista fornecida na atividade
+numeros = [10, 20, 30, 20, 40, 20, 50]
+print(Quantidade de elementos na lista:)
+print( O número 20 aparece vezes na lista.)"
+print(" A posição (índice) do número 30 é")
+print("   O número 100 está na lista?")
---
+++ contagem.py
@@ -0,0 +1,15 @@
+numeros = [10, 20, 30, 40; 50]
+print("lista: ", numeros)
+
+#len: número de elementos
+print("comprimento: ", len(numeoros))
+
+# count: conta corrências
+print("quantas vezes 20 aparece: ", numeros.count(20))
+
+## encontra a posição
+print("índice de 30: ", numeros.index(30))
+
+#in: verifica a existência
+print("20 está na lista? ", 20 in numeros)
+print("100 está na lista? ", 100 in numeros)
